9c51a3485e8a8e3538e32c3f6e08e9d941dc3284
crankycode <crankyfish86@gmail.com>
2015-08-25 18:29:31 +0800

    update build.gradle file build tool to 23.0.0

--- app/src/main/java/com/crankycode/android/mall/CameraFragment.java
+++ app/src/main/java/com/crankycode/android/mall/CameraFragment.java
@@ -1,9 +1,11 @@
 package com.crankycode.android.mall;
 
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +21,9 @@ public class CameraFragment extends android.support.v4.app.Fragment {
     private Camera mCamera;
     private SurfaceView mSurfaceView;
 
-    @Nullable
+
     @Override
+    @SuppressWarnings("deprecation")
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.fragment_camera, parent, false);
 
@@ -32,6 +35,34 @@ public class CameraFragment extends android.support.v4.app.Fragment {
         });
         mSurfaceView = (SurfaceView)v.findViewById(R.id.camera_surfaceView);
 
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // setType() and SURFACE_TYPE_PUSH_BUFFERS are both deprecated,
+        // but are required for Camera preview to work on pre-3.0 devices.
+
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD) {
+            mCamera = Camera.open();
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
